8789715623aea1a9349404ac6f2c057162ac1c7c
sajjad <assassinsajjad0@gmail.com>
2022-03-11 14:32:01 +0330

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Wallet_Payment
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+    }
+}
